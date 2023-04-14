2f0239850f0b2e1b34b9294fa81cc98ba9ccfcc9
bleons36 <89231748+bleons36@users.noreply.github.com>
2023-04-14 14:39:53 +0900

    Delete network.tf

--- network.tf
+++
@@ -1,9 +0,0 @@
-//--------------------------------------------------------------------
-// Modules
-module "network" {
-  source  = "app.terraform.io/hashicat-exper/network/azurerm"
-  version = "5.2.0"
-
-  resource_group_name = "test"
-  use_for_each = "count"
-}
